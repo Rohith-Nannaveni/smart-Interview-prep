e155939de3b8d668218f1496d58d28fd7aff589c
rohit <rohith.nannaveni@gmail.com>
2025-09-12 00:42:42 +0530

    update

--- frontend/interview-prep-ai/src/components/Inputs/ProfilePhotoSelector.jsx
+++ frontend/interview-prep-ai/src/components/Inputs/ProfilePhotoSelector.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { LuUser, LuUpload, LuTrash } from "react-icons/lu";
 
 const profilePhotoSelector = ({ image, setImage, preview, setPreview }) => {
