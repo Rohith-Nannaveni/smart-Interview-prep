8d9f3912afef1f0386db77446189e1bee4e86116
rohit <rohith.nannaveni@gmail.com>
2025-09-20 17:20:06 +0530

    Updating SignUp API

--- frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input";
 import ProfilePhotoSelector from "../../components/Inputs/profilePhotoSelector";
 import { validateEmail } from "../../utils/helper";
+import { UserContext } from "../../context/userContext";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const SignUp = ({ setcurrentPage }) => {
   const [profilePic, setProfilePic] = useState(null);
@@ -11,6 +14,7 @@ const SignUp = ({ setcurrentPage }) => {
   const [password, setPassword] = useState("");
 
   const [error, setError] = useState(null);
+  const { updateUser } = useContext(UserContext)
   const navigate = useNavigate();
 
   // Handle signup Form submit
@@ -37,7 +41,25 @@ const SignUp = ({ setcurrentPage }) => {
     // SignUp API call
 
     try{
-      
+      if(profilePic){
+        const imgUploadRes = await uploadImage(profilePic);
+        profileImageUrl=imgUploadRes.imageUrl || "";
+      }
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        name:fullName,
+        email,
+        password,
+        profileImageUrl,
+      });
+
+      const {token} = response.data;
+
+      if(token){
+        localStorage.setItem("token",token);
+        updateUser(response.data);
+        navigate("/dashboard");
+      }
     } catch(error){
       if(error.response && error.response.data.message){
         setError(error.response.data.message);
