5c00cbac0b8c5fd0376469fb842f900f4d15fa78
rohit <rohith.nannaveni@gmail.com>
2025-09-20 17:37:35 +0530

    Updates

--- frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
@@ -6,6 +6,7 @@ import { validateEmail } from "../../utils/helper";
 import { UserContext } from "../../context/userContext";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
+import uploadImage from "../../utils/uploadImage";
 
 const SignUp = ({ setcurrentPage }) => {
   const [profilePic, setProfilePic] = useState(null);
