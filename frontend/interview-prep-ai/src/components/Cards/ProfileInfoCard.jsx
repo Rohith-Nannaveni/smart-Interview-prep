213e0903dd59d67ecff06af0d67d14ff95f0ab8f
rohit <rohith.nannaveni@gmail.com>
2025-09-20 16:59:52 +0530

    Updated profileInfoCard

---
+++ frontend/interview-prep-ai/src/components/Cards/ProfileInfoCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfileInfoCard = () => {
+  return (
+    <div>
+      ProfileInfoCard
+    </div>
+  )
+}
+
+export default ProfileInfoCard
--- frontend/interview-prep-ai/src/pages/LandingPage.jsx
+++ frontend/interview-prep-ai/src/pages/LandingPage.jsx
@@ -1,20 +1,29 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import HERO_IMG from "../assets/hero-img.png";
 import { APP_FEATURES } from "../utils/data";
-import { useNavigate } from 'react-router-dom'
-import { LuSparkles } from 'react-icons/lu'
+import { useNavigate } from "react-router-dom";
+import { LuSparkles } from "react-icons/lu";
 import Login from "./Auth/Login";
 import SignUp from "./Auth/SignUp";
 import Modal from "../components/Modal";
-
+import { useContext } from "react";
+import { UserContext } from "../context/userContext";
+import ProfileInfoCard from "../components/Cards/ProfileInfoCard";
 
 const LandingPage = () => {
+  const { user } = useContext(UserContext);
   const navigate = useNavigate();
 
   const [openAuthModal, setopenAuthModal] = useState(false);
   const [currentPage, setcurrentPage] = useState("login");
 
-  const handleCTA = () => {};
+  const handleCTA = () => {
+    if (!user) {
+      setopenAuthModal(true);
+    } else {
+      navigate("/dashboard");
+    }
+  };
 
   return (
     <>
@@ -27,11 +36,16 @@ const LandingPage = () => {
             <div className="text-xl text-black font-bold">
               Interview Prep AI
             </div>
-            <button 
-            className="bg-linear-to-r from-[#FF9324] to-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white border border-white transition-colors cursor-pointer" 
-            onClick={() => setopenAuthModal(true)}>
-              Login / Sign Up
-            </button>
+            {user ? (
+              <ProfileInfoCard />
+            ) : (
+              <button
+                className="bg-linear-to-r from-[#FF9324] to-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white border border-white transition-colors cursor-pointer"
+                onClick={() => setopenAuthModal(true)}
+              >
+                Login / Sign Up
+              </button>
+            )}
           </header>
 
           {/* Hero Content */}
@@ -39,24 +53,32 @@ const LandingPage = () => {
             <div className="w-full md:w-1/2 pr-4 mb-8 md:mb-0">
               <div className="flex items-center justify-left mb-2">
                 <div className="flex items-center gap-2 text-[13px] text-amber-600 font-semibold bg-amber-100 px-3 py-1 rounded-full border border-amber-300">
-                  <LuSparkles />AI powered
-                  </div>
+                  <LuSparkles />
+                  AI powered
+                </div>
               </div>
 
               <h1 className="text-5xl text-black font-medium mb-6 leading-tight">
                 Ace Interviews with <br />
-                <span className="text-transparent bg-clip-text bg-[radial-gradient(circle,_#FF9324_0%,_#FCD760_100%)] bg-[length:200%_200%] animate-text-shine font-semibold">AI-Powered</span> Learning
+                <span className="text-transparent bg-clip-text bg-[radial-gradient(circle,_#FF9324_0%,_#FCD760_100%)] bg-[length:200%_200%] animate-text-shine font-semibold">
+                  AI-Powered
+                </span>{" "}
+                Learning
               </h1>
             </div>
 
             <div className="w-full md:w-1/2">
               <p className="text-[17px] text-gray-900 mr-0 md:mr-20 mb-6">
                 Get role-specific questions, expand answers when you need them,
-                dive deeper into concepts, and organize everything your way. From
-                preparation to mastery - your ultimate interview toolkit is here.
+                dive deeper into concepts, and organize everything your way.
+                From preparation to mastery - your ultimate interview toolkit is
+                here.
               </p>
 
-              <button className="bg-black text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-yellow-100 hover:text-black border border-yellow-50 hover:border-yellow-300 transition-colors cursor-pointer" onClick={handleCTA}>
+              <button
+                className="bg-black text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-yellow-100 hover:text-black border border-yellow-50 hover:border-yellow-300 transition-colors cursor-pointer"
+                onClick={handleCTA}
+              >
                 Get Started
               </button>
             </div>
@@ -84,17 +106,16 @@ const LandingPage = () => {
               <div className="flex flex-col items-center gap-8">
                 {/* First 3 cards */}
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full">
-                  {APP_FEATURES.slice(0,3).map((feature) => (
+                  {APP_FEATURES.slice(0, 3).map((feature) => (
                     <div
-                      key ={feature.id}
-                      className="bg-[#FFFEF8] pg-6 rounded-xl shadow-xm hover:shadow-lg shadow-amber-100 transition border border-amber-100" 
+                      key={feature.id}
+                      className="bg-[#FFFEF8] pg-6 rounded-xl shadow-xm hover:shadow-lg shadow-amber-100 transition border border-amber-100"
                     >
                       <h3 className="text-base font-semibold mb-3">
                         {feature.title}
                       </h3>
 
                       <p className="text-gray-600">{feature.description}</p>
-
                     </div>
                   ))}
                 </div>
@@ -103,8 +124,8 @@ const LandingPage = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                   {APP_FEATURES.slice(3).map((feature) => (
                     <div
-                     key={feature.id}
-                     className="bg-[#FFFEF8] pg-6 rounded-xl shadow-xm hover:shadow-lg shadow-amber-100 transition border border-amber-100"
+                      key={feature.id}
+                      className="bg-[#FFFEF8] pg-6 rounded-xl shadow-xm hover:shadow-lg shadow-amber-100 transition border border-amber-100"
                     >
                       <h3 className="text-base font-semibold mb-3">
                         {feature.title}
@@ -117,30 +138,27 @@ const LandingPage = () => {
             </section>
           </div>
         </div>
-        
+
         <div className="text-sm bg-gray-50 text-secondary text-center p-5 mt-5">
           Made with 💙 by Rohith
         </div>
       </div>
 
       <Modal
-        isOpen = {openAuthModal}
-        onClose={() =>{
+        isOpen={openAuthModal}
+        onClose={() => {
           setopenAuthModal(false);
           setcurrentPage("login");
         }}
         hideHeader
-       >
-
+      >
         <div>
-          {currentPage === "login" && (
-            <Login setcurrentPage={setcurrentPage}/>
-          )}
+          {currentPage === "login" && <Login setcurrentPage={setcurrentPage} />}
           {currentPage === "signup" && (
-            <SignUp setcurrentPage={setcurrentPage}/> 
+            <SignUp setcurrentPage={setcurrentPage} />
           )}
         </div>
-      </Modal>   
+      </Modal>
     </>
   );
 };
