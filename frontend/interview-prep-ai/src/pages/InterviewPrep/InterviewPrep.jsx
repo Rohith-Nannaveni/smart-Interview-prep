c7fe6cc35867c4ef0956ed95329c7c3c225a4b05
rohit <rohith.nannaveni@gmail.com>
2025-09-21 23:17:19 +0530

    Questions related

--- frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import moment from "moment";
+import { AnimatePresence, motion } from "framer-motion";
+import { LuCircleAlert, LuListCollapse } from "react-icons/lu";
+import SpinnerLoader from "../../components/Loader/SpinnerLoader";
+import { toast } from "react-hot-toast";
 const InterviewPrep = () => {
-  return (
-    <div>
-      Interview prep
-    </div>
-  )
-}
+  
+  const { sessionId } = useParams();
 
-export default InterviewPrep
+  const [sessionData, setSessionData] = useState(null);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const [openLeanMoreDrawer, setOpenLeanMoreDrawer] = useState(false);
+  const [explanation, setExplanation] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isUpdateLoader, setIsUpdateLoader] = useState(false);
+
+  const fetchSessionDetailsbyId = async () => {};
+
+  const generateConceptExplanation = async (question) => {};
+
+  const toggleQuestionPinStatus = async(questionId) => {};
+
+  const uploadMoreQuestions = async() => {};
+
+  useEffect(() => {
+    if(sessionId){
+      fetchSessionDetailsbyId();
+    }
+  
+    return () => {};
+  }, []);
+  
+  return 
+};
+
+export default InterviewPrep;
